2a205cfc7a42a9487ee65f5458d2bff6dd0519e3
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-21 15:18:49 +0530

    basic routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    return response() -> json([
+        'name' => 'Arjun'
+    ]);
+});
+
+Route::get('/user-page', function () {
+    return view('user');
+});
