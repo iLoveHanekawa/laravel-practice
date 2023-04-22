fef16905a0aabaf6baed9cea18a79d9875861352
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-22 07:25:47 +0530

    echo and verbatim

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,10 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>User Page</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @vite('resources/css/app.css')
     </head>
     <body>
-        <div class = 'py-4 bg-red-300 text-white'>Hello {{ time() }}</div>
+        @verbatim
+            <div class = 'py-4 bg-red-300 text-white'>Hello {{ $name }}</div>
+        @endverbatim
+        <script>
+            const app = {{ Js::from("hello") }}
+            console.log(app)
+        </script>
     </body>
 </html>
